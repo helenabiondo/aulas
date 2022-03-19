9d455d3c152c3326d18e2235f0db380a6cad73c9
helenabiondo <99821535+helenabiondo@users.noreply.github.com>
2022-03-18 21:01:14 -0300

    Update codigo.ino

--- exercicios/exercicio 3/codigo.ino
+++ exercicios/exercicio 3/codigo.ino
@@ -1,1 +1,20 @@
 
+int i = 1;
+void setup ()
+{
+  while (i<6) {
+    pinMode (i,OUTPUT);
+    i = i+1;
+  }
+}
+
+void loop ()
+{
+  for(i=1; i<=5; i++) {
+    digitalWrite (i, HIGH);
+    delay (1000);
+  }
+  
+  delay(10); //deley a litlle bit to improve simulation performance
+}
+  
